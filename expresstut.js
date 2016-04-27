c60b9ca00d8c6d4c5503583f0a23197946bcd64d
Evgeniy B <redrize@yandex.ru>
2016-04-28 01:08:52 +0300

    s maka

--- expresstut.js
+++ expresstut.js
@@ -3,3 +3,27 @@ var express = require('express');
 var app = express();
 
 app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', handlebars);
+
+//More imports here
+
+app.set('port', 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+
+
+
+app.listen(app.get('port'),  function () {
+	console.log("To terminate prec Ctrl+C");
+});
+
+
